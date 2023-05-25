62e1da52a84c093b9996da4d09282173a8b1f7ab
rm88117 <vitoriadasilvag23@gmail.com>
2023-05-25 10:38:16 -0300

    retomada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // Aula introdutória de Orientação à Objetos
+// Declaração de objeto
+        Pessoa adao;
+// Instanciação do objeto
+        adao = new Pessoa();
+// Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+// Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+
+    // Atributos da classe
+    public String nome;
+    public String sobrenome;
+    // métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+}
